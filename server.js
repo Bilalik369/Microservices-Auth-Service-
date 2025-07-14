1588667ae9bac0464306ef3632065a50ddd32831
Bilalik369 <bilal11iken@gmail.com>
2025-07-14 15:39:09 +0100

    connexion avec base des donnees

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
 
-
+connectDB();
 app.listen(PORT, ()=>{
     console.log(`server is rannuing in the port${PORT}`)
 })
