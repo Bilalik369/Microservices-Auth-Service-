b5280e00d405361eb0ec8d4fdff2b5270072d614
Bilalik369 <bilal11iken@gmail.com>
2025-07-15 15:05:28 +0100

    ajoute la fintion de login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -43,3 +43,43 @@ export const register = async(req , res)=>{
         res.status(500).json({msg : "error de linscription"})
     }
 }
+
+
+
+export const login = async(req ,res)=>{
+    try {
+        const {email , password} =req.body;
+        if(!email || !password){
+            return res.status(400).json({msg : "tout les champs est obligatoirer"})
+        }
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({msg : "Email ou mot de passe incorrect"})
+        }
+        const isPasswordValide = await user.comparePassword(password)
+        if(!isPasswordValide){
+            return res.status(401).json({msg : "password in valide"})
+        } 
+        if(!user.isActive){
+            return res.status(401).json({msg : "Compte désactivé"})
+        }
+
+        const token = user.generateAuthToken()
+
+
+        res.status(201).json({
+            success : true, 
+            message : "login success",
+            token,
+            user : {
+                id : user._id,
+                nom: user.nom,
+                email: user.email,
+                role: user.role
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({msg : "Erreur serveur lors de la connexion"})
+    }
+}
--- models/User.model.js
+++ models/User.model.js
@@ -49,6 +49,8 @@ userSchema.pre("save" , async function (next) {
 userSchema.methods.generateAuthToken = function (){
     return jwt.sign({id : this._id , role : this.role}, process.env.JWT_SECRET  , { expiresIn: process.env.JWT_EXPIRES_IN})
 }
-
+userSchema.methods.comparePassword =async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword , this.password)
+}
 
 export default mongoose.model("User" , userSchema)
